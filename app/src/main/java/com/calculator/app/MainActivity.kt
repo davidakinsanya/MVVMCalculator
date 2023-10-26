98c20a24aaa00bf3e5f3c03bcf1147fb6c5669b7
davidakinsanya <davidakinsanya00@gmail.com>
2023-10-26 11:35:09 +0100

    Updating libraries

--- app/src/main/java/com/calculator/app/MainActivity.kt
+++ app/src/main/java/com/calculator/app/MainActivity.kt
@@ -1,40 +1,35 @@
 package com.calculator.app
 
+import android.annotation.SuppressLint
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.calculator.app.ui.theme.CalculatorTheme
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
+
+@OptIn(ExperimentalAnimationApi::class)
 class MainActivity : ComponentActivity() {
+  @SuppressLint("SourceLockedOrientationActivity")
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    this.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
     setContent {
-      CalculatorTheme {
-        // A surface container using the 'background' color from the theme
-        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-          Greeting("Android")
-        }
-      }
+      val navController = rememberAnimatedNavController()
+      val systemUiController = rememberSystemUiController()
+      SideEffect { systemUiController.setSystemBarsColor(color = Color(0xFFFFFFFF)) }
     }
-  }
-}
-
-@Composable
-fun Greeting(name: String) {
-  Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-  CalculatorTheme {
-    Greeting("Android")
   }
 }
