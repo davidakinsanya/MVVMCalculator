f27ee702513072d385b4029300ca6a135a217d0a
davidakinsanya <davidakinsanya00@gmail.com>
2023-10-26 12:01:21 +0100

    Updating libraries

--- app/src/main/java/com/calculator/app/MainActivity.kt
+++ app/src/main/java/com/calculator/app/MainActivity.kt
@@ -6,16 +6,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import com.calculator.app.ui.theme.CalculatorTheme
 import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
